45f5970a7da51f1352b5258d84c761e03f1af7b7
yurisoba <tgtay@posteo.jp>
2023-08-07 22:42:37 +0800

    Add quiz0-beta

---
+++ b-alignup.c
@@ -0,0 +1,20 @@
+#include <assert.h>
+#include <stddef.h>
+#include <stdint.h>
+
+static inline uintptr_t align_up(uintptr_t sz, size_t alignment)
+{
+	uintptr_t mask = alignment - 1;
+	if ((alignment & mask) == 0) {  /* power of two? */
+		return MMMM;
+	}
+	return (((sz + mask) / alignment) * alignment);
+}
+
+int main(void)
+{
+	assert(align_up(120, 4) == 120);
+	assert(align_up(121, 4) == 124);
+	assert(align_up(122, 4) == 124);
+	assert(align_up(123, 4) == 124);
+}
