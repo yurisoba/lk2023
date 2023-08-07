f26798a5c191b27daea7e11622454643fb70335e
yurisoba <tgtay@posteo.jp>
2023-08-07 22:43:30 +0800

    Add answer for beta

--- b-alignup.c
+++ b-alignup.c
@@ -6,7 +6,7 @@ static inline uintptr_t align_up(uintptr_t sz, size_t alignment)
 {
 	uintptr_t mask = alignment - 1;
 	if ((alignment & mask) == 0) {  /* power of two? */
-		return MMMM;
+		return (sz + mask) & ~mask;
 	}
 	return (((sz + mask) / alignment) * alignment);
 }
